d8866a4169fd10ae6d43bc039357c2da40f75afd
Joanna Bukowska-Chlebus <jb.chlebus@gmail.com>
2024-07-10 10:53:08 +0200

    2.5.1 ćwiczenie pętle

---
+++ kodilla-intro/src/main/java/Application.java
@@ -0,0 +1,7 @@
+public class Application {
+    public static void main(String[] args) {
+        CodeRefactoring.User user = new CodeRefactoring.User("Adam", 40.5, 178);
+        user.printUserInfo();
+    }
+}
+
---
+++ kodilla-intro/src/main/java/CodeRefactoring.java
@@ -0,0 +1,25 @@
+public class CodeRefactoring {
+    static class User {
+        String name;
+        double age;
+        double height;
+
+        public User(String name, double age, double height) {
+            this.name = name;
+            this.age = age;
+            this.height = height;
+        }
+
+        public void printUserInfo() {
+            if (name != null) {
+                if (age > 30 && height > 160) {
+                    System.out.println("User is older than 30 and taller than 160cm");
+                } else {
+                    System.out.println("User is 30 (or younger) or 160cm (or shorter)");
+                }
+            }
+        }
+    }
+}
+
+
